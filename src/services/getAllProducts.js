3b4e92a4e818553bc50a2b46703ba37a62713d0c
Azzahraaini12 <azzahraaini.22008@mhs.unesa.ac.id>
2024-11-18 13:28:55 +0700

    react azzahra

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,93 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'NAF-1-07-FWOMEN-001',
+      slug: 'kemeja-wanita1',
+      name: "WS KAUWELA SAFARI SHIRT",
+      category: `Women's Shirt`,
+      categorySlug: 'women-shirt',
+      price: 549_000,
+      stock: 100,
+      imageUrl: '/assets/images/WSKAUWELASAFARISHIRT1.jpeg',
+      description: `Jelajahi destinasi wisata penuh percaya diri dengan mengenakan WS Kauwela Safari Shirt. Hadir dalam tampilan klasik dengan bahan katun yang lembut untuk memberikan kenyamanan sepanang hari. Kemeja lengan panjang wanita ini dilengkapi dua saku untuk menyimpan barang-barang agar tetap aman saat bepergian. Baik untuk hari yang panjang di kantor atau bermain di luar ruangan, kemeja ini akan menjadi andalanmu.`
+    },
+    {
+      id: 'NAF-1-07-FMEN-002',
+      slug: 'kemeja-pria1',
+      name: "SONOMA FLANNEL SHIRT",
+      category: `Men's Shirt`,
+      categorySlug: 'men-shirt',
+      price: 920_000,
+      stock: 5,
+      imageUrl: `/assets/images/SONOMAFLANNELSHIRT2.jpeg`,
+      description: `Sempurnakan penampilanmu dengan Sonoma Flannel, kemeja yang cocok untuk menemani aktivitas harian. Hadir dengan gaya klasik motif kotak-kotak, serta dilengkapi dua saku di bagian dada dengan penutup kancing. Kemeja lengan panjang ini pas untuk dipadukan dengan celana apa pun untuk melengkapi tampilan kasual.`
+    },
+    {
+      id: 'NAF-1-07-FMEN-003',
+      slug: 'kemeja-pria2',
+      name: "FLOBAMORA EXP SS",
+      category: `Men's Shirt`,
+      categorySlug: 'men-shirt',
+      price: 580_000,
+      stock: 0,
+      imageUrl: `/assets/images/FLOBAMORAEXPSS3.jpeg`,
+      description: `Kemeja lengan pendek Flobamora Exp membuat kamu tetap sejuk dan nyaman saat mendaki, bahkan di hari yang panas. Terbuat dari bahan poliester yang dilengkapi teknologi Tropic Dry untuk menyerap kelembapan dan ventilasi di bagian punggung untuk menjaga tubuh tetap kering saat berkeringat.`
+    },
+    {
+      id: 'NAF-1-07-FWOMEN-004',
+      slug: 'kemeja-wanita2',
+      name: "WS SKIDDAW LS SHIRT",
+      category: `Women's Shirt`,
+      categorySlug: 'women-shirt',
+      price: 909_000,
+      stock: 100 ,
+      imageUrl: `/assets/images/WSSKIDDAWLSSHIRT4.jpeg`,
+      description: `Women Skiddaw Long Sleeves Shirt hadir untuk mendukung kamu di berbagai medan pendakian. Kemeja lengan panjang yang fungsional ini dilengkapi teknologi Tropic Dry serta ventilasi udara di bagian punggung, memastikan tubuh tetap kering dan sejuk saat berkeringat.`
+    },
+    {
+      id: 'NAF-1-07-FMEN-005',
+      slug: 'kemeja-pria3',
+      name: "TROY ROUTE WORK SHIRT 2.0",
+      category: `Men's Shirt`,
+      categorySlug: 'men-shirt',
+      price: 500_000,
+      stock: 100,
+      imageUrl: '/assets/images/TROYROUTEWORKSHIRT5.jpeg',
+      description: `Tampil rapi dengan Troy Route Work Shirt 2.0 saat berkendara atau beraktivitas sehari-hari. Kemeja dari EIGER Riding ini memiliki potongan yang pas di tubuh sekaligus memberikan kesan rapi saat berkendara. Dengan tambahan saku di bagian dada, kemeja ini juga praktis untuk menyimpan barang-barang kecil saat berkendara.`
+    },  
+    {
+      id: 'NAF-1-07-FMEN-006',
+      slug: 'kemeja-pria4',
+      name: "TADULOKA SS F1",
+      category: `Men's Shirt`,
+      categorySlug: 'men-shirt',
+      price: 649_000,
+      stock: 100,
+      imageUrl: '/assets/images/TADULOKASSF16.jpeg',
+      description: `Kemeja Taduloka SS F1 menemani kamu saat menjalani aktivitas outdoor. Bebas dari gerah lebih cepat dengan kombinasi teknologi Tropic Dry dan sistem ventilasi pada punggung yang memastikan kain lebih cepat kering dan sirkulasi udara lebih baik.`
+    },
+    {
+      id: 'NAF-1-07-FWOMEN-007',
+      slug: 'kemeja-wanita3',
+      name: "ARCANE",
+      category: `Women's Shirt`,
+      categorySlug: 'women-shirt',
+      price: 389_000,
+      stock: 10,
+      imageUrl: '/assets/images/ARCANE7.jpeg',
+      description: `Arcane menyempurnakan style kasual Anda dengan potongan oversized dan corak bergaris yang artsy. Bahan katun poplin yang nyaman membuat kemeja lengan panjang ini selalu pas untuk outfit harian maupun momen weekend.`
+    },
+    {
+      id: 'NAF-1-07-FWOMEN-008',
+      slug: 'kemeja-wanita4',
+      name: "WS GARNET LS SHIRT",
+      category: `Women's Shirt`,
+      categorySlug: 'women-shirt',
+      price: 479_000,
+      stock: 100,
+      imageUrl: '/assets/images/WSGARNETLSSHIRT8.jpeg',
+      description: `Pendakian panjang atau petualangan di hutan - apapun rencanamu, Garnet Long Sleeve Shirt akan menjadi teman yang baik dalam perjalanan. Kemeja lengan panjang yang ringan dan nyaman untuk trekking atau aktivitas luar ruangan di musim kemarau. Bahannya yang lembut dan cepat kering membuat pendakian menjadi lebih nyaman.`
+    },
+
+  ]
+}
